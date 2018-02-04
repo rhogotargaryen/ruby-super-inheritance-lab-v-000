88bfb3e62f0737b0fbca1f7f2a1306ddc3d21526
Derek Haller <dhaller.personal@gmail.com>
2018-02-04 07:18:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
   def hello
-    return "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
   def raise_hand
-    return "Pick me!"
+    puts "Pick me!"
   end
 end
